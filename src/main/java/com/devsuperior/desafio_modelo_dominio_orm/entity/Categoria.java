488d9aeec66312f9635718c641d0ad8f75193280
Bruno Nascimento <brunowcnascimento@gmail.com>
2024-11-20 21:26:35 -0300

    Update Categoria.java

--- src/main/java/com/devsuperior/desafio_modelo_dominio_orm/entity/Categoria.java
+++ src/main/java/com/devsuperior/desafio_modelo_dominio_orm/entity/Categoria.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "tb_categoria")
 public class Categoria {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
