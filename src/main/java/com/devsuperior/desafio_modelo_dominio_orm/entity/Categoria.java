55365e5fb9132db7db6eae94b1c2e03582566e8a
Bruno Nascimento <brunowcnascimento@gmail.com>
2024-11-20 21:39:56 -0300

    Update Categoria.java

--- src/main/java/com/devsuperior/desafio_modelo_dominio_orm/entity/Categoria.java
+++ src/main/java/com/devsuperior/desafio_modelo_dominio_orm/entity/Categoria.java
@@ -2,6 +2,9 @@ package com.devsuperior.desafio_modelo_dominio_orm.entity;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
+import java.util.Set;
+
 @Entity
 @Table(name = "tb_categoria")
 public class Categoria {
@@ -9,6 +12,47 @@ public class Categoria {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String descricao;
+
+    @OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
+    private Set<Atividade> atividades;
+
+    public Categoria() {}
+
+    public Categoria(Long id, String descricao) {
+        this.id = id;
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Set<Atividade> getAtividades() {
+        return atividades;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Categoria categoria)) return false;
+        return Objects.equals(id, categoria.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
