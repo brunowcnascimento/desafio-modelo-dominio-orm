de149888c6c1668207e95fbc296769a7ad9f7c1e
Bruno Nascimento <brunowcnascimento@gmail.com>
2024-11-20 21:25:07 -0300

    Create Categoria.java

---
+++ src/main/java/com/devsuperior/desafio_modelo_dominio_orm/entity/Categoria.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafio_modelo_dominio_orm.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String descricao;
+}
