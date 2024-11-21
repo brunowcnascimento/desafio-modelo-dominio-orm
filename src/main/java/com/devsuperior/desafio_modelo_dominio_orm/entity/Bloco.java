1e1e87e267b81372686fdf94392db4bbdfb2fe3b
Bruno Nascimento <brunowcnascimento@gmail.com>
2024-11-20 21:27:26 -0300

    Create Bloco.java

---
+++ src/main/java/com/devsuperior/desafio_modelo_dominio_orm/entity/Bloco.java
@@ -0,0 +1,20 @@
+package com.devsuperior.desafio_modelo_dominio_orm.entity;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_bloco")
+public class Bloco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant inicio;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant fim;
+}
